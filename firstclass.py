73a55e47283bb2f686253d6cbff904134d825a9b
typescriptkamran <typescriptwithkamran@gmail.com>
2023-10-17 18:29:09 +0500

    first commit

---
+++ firstclass.py
@@ -0,0 +1,15 @@
+print("Pakistan Zindabad")
+myname = "muhammad Kamran"
+myname: str = "muhammad Kamran"
+hisname: str = "700"
+
+# to install mytype in anaconda please check this repository
+# https://github.com/conda-forge/mypy-feedstock
+# conda install mypy mypyc
+
+name = "muhammad Kamran"
+print("name")
+print(type(name))
+print(id(name))
+print([i for i in dir(name) if "__" not in i]) # method attributes
+
